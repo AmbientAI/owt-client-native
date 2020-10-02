6e5720d640513694b6562e4ac4951778ae9f1466
Benjamin Shapero <bshapero@ambient.ai>
2020-10-02 15:48:47 -0700

    Fix typo in for loop.

--- talk/owt/sdk/p2p/p2pclient.cc
+++ talk/owt/sdk/p2p/p2pclient.cc
@@ -86,11 +86,11 @@ void P2PClient::RemoveAllowedRemoteId(const std::string& target_id,
       }
       return;
     }
-    for (auto it = allowed_remote_ids_.begin(), it != allowed_remote_ids_.end();) {
+    for (auto it = allowed_remote_ids_.begin(); it != allowed_remote_ids_.end();) {
       if (*it == target_id) {
         allowed_remote_ids_.erase(it);
       } else {
-        it++;
+        ++it;
       }
     }
   }
@@ -400,11 +400,11 @@ void P2PClient::OnStopped(const std::string& remote_id) {
   // remove from allowed ids to prevent memory leaks.
   {
     const std::lock_guard<std::mutex> lock(remote_ids_mutex_);
-    for (auto it = allowed_remote_ids_.begin(), it != allowed_remote_ids_.end();) {
+    for (auto it = allowed_remote_ids_.begin(); it != allowed_remote_ids_.end();) {
       if (*it == remote_id) {
         allowed_remote_ids_.erase(it);
       } else {
-        it++;
+        ++it;
       }
     }
   }
